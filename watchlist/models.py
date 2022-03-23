fdfa680ea831f93297e9a3c3d75980897188bc93
zhujiace <zhujiace020612@gmail.com>
2022-03-23 23:22:09 +0800

    ready

---
+++ watchlist/models.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from enum import unique
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from watchlist import db
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20))
+    username = db.Column(db.String(20), unique=True, index=True)    # unique确保不会有重复值 index建立索引提高用此项检索用户的效率
+    password_hash = db.Column(db.String(128))
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def validate_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(60))
+    year = db.Column(db.String(4))
+
+class Beat(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(40))
